6f5566f96cd67f415fc1d5a6950755b3a792ea4a
dygma0 <404err@naver.com>
2023-09-30 20:16:53 +0900

    📦️ gson 의존성 추가

--- tldr-api/src/main/kotlin/run/cd80/tldr/api/config/ClientConfig.kt
+++ tldr-api/src/main/kotlin/run/cd80/tldr/api/config/ClientConfig.kt
@@ -2,6 +2,8 @@ package run.cd80.tldr.api.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import run.cd80.tldr.core.calendar.Calendar
+import run.cd80.tldr.core.calendar.impl.StandardCalendar
 import run.cd80.tldr.core.http.HttpClient
 import run.cd80.tldr.core.http.impl.FuelHttpClient
 
@@ -11,5 +13,10 @@ class ClientConfig {
     @Bean
     fun httpClient(): HttpClient {
         return FuelHttpClient()
+    }
+
+    @Bean
+    fun calendar(): Calendar {
+        return StandardCalendar()
     }
 }
