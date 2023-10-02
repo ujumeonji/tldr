6f54f026ad442a3aa386e31e86484ae557b3ee13
dygma0 <404err@naver.com>
2023-10-02 21:33:45 +0900

    ✅ createBlob 테스트 추가

--- tldr-api/src/test/kotlin/run/cd80/tldr/api/manager/github/GithubManagerTest.kt
+++ tldr-api/src/test/kotlin/run/cd80/tldr/api/manager/github/GithubManagerTest.kt
@@ -3,6 +3,7 @@ package run.cd80.tldr.api.manager.github
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import run.cd80.tldr.api.config.GithubConfig
+import run.cd80.tldr.api.manager.github.dto.CreateBlob
 import run.cd80.tldr.api.manager.github.vo.GitRepository
 import run.cd80.tldr.api.manager.github.vo.GithubAccessToken
 import run.cd80.tldr.api.manager.github.vo.GithubCode
@@ -63,7 +64,7 @@ class GithubManagerTest : DescribeSpec({
                             "url": "https://api.github.com/repos/dygma0/git-test/git/commits/4eb26e0f47059b222a4a44c0cbd6c8b47d1998d4"
                           }
                         }
-                    """.trimIndent()
+                    """.trimIndent(),
                 ),
             )
 
@@ -81,6 +82,34 @@ class GithubManagerTest : DescribeSpec({
             result.`object`.sha shouldBe "4eb26e0f47059b222a4a44c0cbd6c8b47d1998d4"
             result.`object`.type shouldBe "commit"
             result.`object`.url shouldBe "https://api.github.com/repos/dygma0/git-test/git/commits/4eb26e0f47059b222a4a44c0cbd6c8b47d1998d4"
+        }
+    }
+
+    describe("createBlob") {
+        it("blob 생성") {
+            // given
+            httpClient.addResponse(
+                HttpResponse(
+                    200,
+                    """
+                        {
+                          "sha": "ee13cb732d05301c7a514512450b572db10293c8",
+                          "url": "https://api.github.com/repos/dygma0/git-test/git/blobs/ee13cb732d05301c7a514512450b572db10293c8"
+                        }
+                    """.trimIndent(),
+                ),
+            )
+
+            // when
+            val result = githubManager.createBlob(
+                CreateBlob.Command("test-content", "repo/path", "test-encoding"),
+                GitRepository.of("test/repo"),
+                GithubAccessToken.of("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"),
+            )
+
+            // then
+            result.sha shouldBe "ee13cb732d05301c7a514512450b572db10293c8"
+            result.url shouldBe "https://api.github.com/repos/dygma0/git-test/git/blobs/ee13cb732d05301c7a514512450b572db10293c8"
         }
     }
 })
