e841776e704ef4597d633e03d97aad9713a1a839
dygma0 <404err@naver.com>
2023-10-02 21:14:18 +0900

    ✅ getReference 테스트 추가

--- tldr-api/src/test/kotlin/run/cd80/tldr/api/manager/github/GithubManagerTest.kt
+++ tldr-api/src/test/kotlin/run/cd80/tldr/api/manager/github/GithubManagerTest.kt
@@ -1,42 +1,86 @@
 package run.cd80.tldr.api.manager.github
 
-import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import run.cd80.tldr.api.config.GithubConfig
+import run.cd80.tldr.api.manager.github.vo.GitRepository
 import run.cd80.tldr.api.manager.github.vo.GithubAccessToken
 import run.cd80.tldr.api.manager.github.vo.GithubCode
 import run.cd80.tldr.core.http.dto.HttpResponse
 import run.cd80.tldr.fixture.StubHttpClient
 import run.cd80.tldr.fixture.StubHttpClientFactory
 
-class GithubManagerTest : BehaviorSpec({
+class GithubManagerTest : DescribeSpec({
 
-    given("a github manager") {
-        val config = GithubConfig()
-        config.clientId = "test-client-id"
-        config.clientSecret = "test-client-secret"
-        config.redirectUri = "http://test:8080"
-        config.scopes = listOf("repo")
+    val httpClient = StubHttpClient()
 
-        val httpClient = StubHttpClient()
-        httpClient.addResponse(
-            HttpResponse(
-                200,
-                "access_token=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&scope=repo&token_type=bearer",
-            ),
-        )
+    val githubManager = GithubManager(
+        StubHttpClientFactory(httpClient),
+        GithubConfig().apply {
+            clientId = "test-client-id"
+            clientSecret = "test-client-secret"
+            redirectUri = "http://test:8080"
+            scopes = listOf("repo")
+        },
+    )
 
-        val githubManager = GithubManager(
-            StubHttpClientFactory(httpClient),
-            config,
-        )
+    beforeTest {
+        httpClient.clear()
+    }
 
-        `when`("getDurations를 호출한 경우") {
+    describe("getAccessToken") {
+        it("access token을 가져온다.") {
+            // given
+            httpClient.addResponse(
+                HttpResponse(
+                    200,
+                    "access_token=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&scope=repo&token_type=bearer",
+                ),
+            )
+
+            // when
             val result = githubManager.getAccessToken(GithubCode.of("test-code"))
 
-            then("return access token") {
-                result shouldBe GithubAccessToken.of("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")
-            }
+            // then
+            result shouldBe GithubAccessToken.of("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")
+        }
+    }
+
+    describe("getReference") {
+        it("특정 저장소에 대한 레퍼런스를 가져온다.") {
+            // given
+            httpClient.addResponse(
+                HttpResponse(
+                    200,
+                    """
+                        {
+                          "ref": "refs/heads/main",
+                          "node_id": "REF_kwDOKa3xU69yZWZzL2hlYWRzL21haW4",
+                          "url": "https://api.github.com/repos/dygma0/git-test/git/refs/heads/main",
+                          "object": {
+                            "sha": "4eb26e0f47059b222a4a44c0cbd6c8b47d1998d4",
+                            "type": "commit",
+                            "url": "https://api.github.com/repos/dygma0/git-test/git/commits/4eb26e0f47059b222a4a44c0cbd6c8b47d1998d4"
+                          }
+                        }
+                    """.trimIndent()
+                ),
+            )
+
+            // when
+            val result = githubManager.getReference(
+                "test-repository",
+                GitRepository.of("test/repo"),
+                GithubAccessToken.of("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"),
+            )
+
+            // then
+            result.url shouldBe "https://api.github.com/repos/dygma0/git-test/git/refs/heads/main"
+            result.ref shouldBe "refs/heads/main"
+            result.nodeId shouldBe "REF_kwDOKa3xU69yZWZzL2hlYWRzL21haW4"
+            result.`object`.sha shouldBe "4eb26e0f47059b222a4a44c0cbd6c8b47d1998d4"
+            result.`object`.type shouldBe "commit"
+            result.`object`.url shouldBe "https://api.github.com/repos/dygma0/git-test/git/commits/4eb26e0f47059b222a4a44c0cbd6c8b47d1998d4"
         }
     }
 })
