237d28c33ec1fecaf09635e214056b79bca45642
dygma0 <404err@naver.com>
2023-10-05 12:19:31 +0900

    🔧 Redis config 수정

--- tldr-api/src/main/kotlin/run/cd80/tldr/api/config/RedisConfig.kt
+++ tldr-api/src/main/kotlin/run/cd80/tldr/api/config/RedisConfig.kt
@@ -4,19 +4,29 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
+
 
 @Configuration
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400)
 class RedisConfig(
-    @Value("\${spring.redis.host}")
+    @Value("\${spring.data.redis.host}")
     private val host: String,
-    @Value("\${spring.redis.port}")
+    @Value("\${spring.data.redis.port}")
     private val port: Int,
+    @Value("\${spring.data.redis.password}")
+    private val password: String,
 ) {
 
     @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory =
-        LettuceConnectionFactory(host, port)
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration()
+        redisStandaloneConfiguration.hostName = host
+        redisStandaloneConfiguration.port = port
+        redisStandaloneConfiguration.setPassword(password);
+
+        return LettuceConnectionFactory(redisStandaloneConfiguration)
+    }
 }
