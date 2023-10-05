16bf17a3966666485a859f4bc08482a7d09993b8
dygma0 <404err@naver.com>
2023-10-05 19:29:25 +0900

    ♻️ OAuth2 리팩토링

--- tldr-api/src/main/java/run/cd80/tldr/api/domain/BaseEntity.java
+++ tldr-api/src/main/java/run/cd80/tldr/api/domain/BaseEntity.java
@@ -17,6 +17,7 @@ public class BaseEntity {
   private LocalDateTime createdAt;
 
   @LastModifiedDate
+  @Column(updatable = false)
   private LocalDateTime updatedAt;
 
   private LocalDateTime deletedAt;
--- tldr-api/src/main/kotlin/run/cd80/tldr/api/config/handler/OAuth2AuthenticationSuccessHandler.kt
+++ tldr-api/src/main/kotlin/run/cd80/tldr/api/config/handler/OAuth2AuthenticationSuccessHandler.kt
@@ -3,7 +3,7 @@ package run.cd80.tldr.api.config.handler
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.Authentication
-import org.springframework.security.oauth2.core.user.OAuth2User
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
 import org.springframework.stereotype.Component
 import run.cd80.tldr.api.base.WorkflowScenario
@@ -19,15 +19,21 @@ class OAuth2AuthenticationSuccessHandler(
         response: HttpServletResponse?,
         authentication: Authentication?,
     ) {
-        val principal = authentication?.principal as OAuth2User?
+        val principal = authentication?.principal as DefaultOAuth2User?
         findOrCreateAccountWorkflow.execute(createRequest(principal))
 
         super.onAuthenticationSuccess(request, response, authentication)
     }
 
-    private fun createRequest(principal: OAuth2User?): FindOrCreateAccount.Request =
-        FindOrCreateAccount.Request(
+    private fun createRequest(principal: DefaultOAuth2User?): FindOrCreateAccount.Request {
+        val emailVerified = principal?.attributes?.get("email_verified") as Boolean? ?: false
+        if (!emailVerified) {
+            throw RuntimeException("Email not verified")
+        }
+
+        return FindOrCreateAccount.Request(
             email = principal?.attributes?.get("email") as String? ?: "",
-            identifier = principal?.attributes?.get("identifier") as String? ?: "",
+            identifier = principal?.name ?: "",
         )
+    }
 }
