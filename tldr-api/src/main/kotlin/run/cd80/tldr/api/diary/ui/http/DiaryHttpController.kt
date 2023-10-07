51bcb4dda6e0c316027753252128c0aad5f09b6f
dygma0 <404err@naver.com>
2023-10-08 00:37:15 +0900

    feat: DailyHttpController 추가

---
+++ tldr-api/src/main/kotlin/run/cd80/tldr/api/diary/ui/http/DiaryHttpController.kt
@@ -0,0 +1,17 @@
+package run.cd80.tldr.api.diary.ui.http
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import run.cd80.tldr.api.diary.ui.http.dto.DailyCalendar
+import java.time.LocalDateTime
+
+@RequestMapping("/diaries")
+@RestController
+class DiaryHttpController {
+
+    @GetMapping("/calendar")
+    fun getDiaries(
+        dailyCalendar: DailyCalendar.Request
+    ): DailyCalendar.Response = DailyCalendar.Response(LocalDateTime.now(), listOf())
+}
---
+++ tldr-api/src/main/kotlin/run/cd80/tldr/api/diary/ui/http/dto/DailyCalendar.kt
@@ -0,0 +1,22 @@
+package run.cd80.tldr.api.diary.ui.http.dto
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+object DailyCalendar {
+
+    data class Request(
+        val month: String,
+    )
+
+    data class Response(
+        val date: LocalDateTime,
+        val diaries: List<Diary>,
+    )
+
+    data class Diary(
+        val id: Long,
+        val title: String,
+        val createdAt: LocalDate
+    )
+}
