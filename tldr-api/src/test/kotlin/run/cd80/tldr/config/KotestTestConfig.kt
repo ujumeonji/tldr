cd8811aff1c9a49b40dd48aaf99e33037427f29f
dygma0 <404err@naver.com>
2023-10-12 21:03:35 +0900

    refactor: Rename kotest configuration class

--- tldr-api/src/test/kotlin/run/cd80/tldr/config/KotestTestConfig.kt
+++ tldr-api/src/test/kotlin/run/cd80/tldr/config/KotestTestConfig.kt
@@ -2,8 +2,10 @@ package run.cd80.tldr.config
 
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.extensions.spring.SpringExtension
+import org.springframework.boot.test.context.TestConfiguration
 
-class KotestConfig : AbstractProjectConfig() {
+@TestConfiguration
+class KotestTestConfig : AbstractProjectConfig() {
 
     override fun extensions() = listOf(SpringExtension)
 }
