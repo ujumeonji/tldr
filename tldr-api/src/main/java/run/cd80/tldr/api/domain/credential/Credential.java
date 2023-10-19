cd4324f0276ef636f0c4a78c3ffe553a95ac3eb5
dygma0 <404err@naver.com>
2023-10-19 23:52:54 +0900

    test: 인가 정보 영속성 레이어 테스트 추가

--- tldr-api/src/main/java/run/cd80/tldr/api/domain/credential/Credential.java
+++ tldr-api/src/main/java/run/cd80/tldr/api/domain/credential/Credential.java
@@ -2,9 +2,8 @@ package run.cd80.tldr.api.domain.credential;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,27 +11,36 @@ import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.Getter;
+import java.time.LocalDateTime;
 import run.cd80.tldr.api.domain.BaseEntity;
 import run.cd80.tldr.api.domain.user.Account;
 
-@Getter
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn(name = "provider")
+@DiscriminatorColumn(name = "provider", discriminatorType = DiscriminatorType.STRING)
 public class Credential extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id", nullable = false)
   private Long id;
-
-  @Enumerated(EnumType.STRING)
-  private Provider provider;
 
   @ManyToOne
   @JoinColumn(name = "account_id", nullable = false)
   private Account account;
+
+  protected Credential() {
+  }
+
+  public Credential(Long id, Account account, LocalDateTime createdAt) {
+    this.id = id;
+    this.account = account;
+    this.setCreatedAt(createdAt);
+    this.setUpdatedAt(createdAt);
+  }
+
+  public Long getId() {
+    return id;
+  }
 
   protected void setAccount(Account account) {
     this.account = account;
